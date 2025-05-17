d1eb00a5c503769f4b2aa5bce938e857d7c9f306
jestxfot <87380272+jestxfot@users.noreply.github.com>
2025-05-17 17:18:01 +0300

    дев5.5

--- dist/bin/bystro292_3.bat
+++ dist/bin/bystro292_3.bat
@@ -1,8 +1,4 @@
 @echo off
-REM Стратегия YTDisBystro 2.9.2 v3
-REM VERSION: 1.1
-REM Дата обновления: 2025-04-27
-
 whoami /groups | find "S-1-5-32-544" >nul 2>&1 && goto :ADMIN
 powershell -nop -c "Start-Process '%~f0' -arg @('ELEV','%*') -Verb RunAs"
 exit /b
@@ -23,7 +19,7 @@ start "---] zapret: http,https,quic,youtube,discord [---" /b "winws.exe" ^
 --filter-tcp=80,443 --hostlist="mycdnlist.txt" --dpi-desync=fakeddisorder --dpi-desync-split-pos=2,midsld --dpi-desync-fakedsplit-pattern="tls_clienthello_1.bin" --dpi-desync-fooling=badseq --new ^
 --filter-tcp=443 --hostlist="discord.txt" --dpi-desync=fakedsplit --dpi-desync-split-pos=midsld-1,1 --dpi-desync-fooling=badseq --dpi-desync-fakedsplit-pattern="tls_clienthello_4.bin" --dpi-desync-autottl --new ^
 --filter-tcp=80 --dpi-desync=fake,multisplit --dpi-desync-split-seqovl=1 --dpi-desync-split-pos=sld+1 --dpi-desync-fooling=badseq --new ^
---filter-tcp=443 --dpi-desync=fakedsplit --dpi-desync-fooling=badseq --dpi-desync-split-pos=2,midsld-1 --dpi-desync-fakedsplit-pattern="tls_clienthello_4.bin" --new ^
+--filter-tcp=443 --hostlist-exclude="netrogat.txt" --dpi-desync=fakedsplit --dpi-desync-fooling=badseq --dpi-desync-split-pos=2,midsld-1 --dpi-desync-fakedsplit-pattern="tls_clienthello_4.bin" --new ^
 --filter-udp=443 --hostlist="discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="quic_1.bin" --new ^
 --filter-udp=50000-50090 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
 --filter-tcp=443 --ipset-ip=XXX.XXX.XXX.XXX/XX,XXX.XXX.XXX.XXX/XX --wssize=1:6 --hostlist-domains=googlevideo.com --dpi-desync=multidisorder --dpi-desync-split-seqovl=1 --dpi-desync-split-pos=1,host+2,sld+2,sld+5,sniext+1,sniext+2,endhost-2 --new ^
